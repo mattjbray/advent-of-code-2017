0c025b8ddea4b54efd93379283d6f637c3ddbc61
Matt Bray <mattjbray@gmail.com>
2017-12-03 19:32:11 +0000

    Solve day 2 part 2.

--- src/advent_of_code_2017/day_2.clj
+++ src/advent_of_code_2017/day_2.clj
@@ -1,16 +1,29 @@
 (ns advent-of-code-2017.day-2)
 
-(defn rowDiff
+(defn row-diff
   [row]
   (- (apply max row)
      (apply min row)))
 
 (defn checksum
   [input]
-  (reduce + (map rowDiff input)))
+  (reduce + (map row-diff input)))
 
 (defn parse-input
   [input]
   (map #(let [strings (clojure.string/split % #"\s+")]
           (map (fn [s] (Integer/parseInt s)) strings))
        (clojure.string/split-lines input)))
+
+(defn row-quotient
+  [row]
+  (first
+   (for [dividend row
+         divisor row
+         :when (and (not= dividend divisor)
+                    (= 0 (rem dividend divisor)))]
+     (quot dividend divisor))))
+
+(defn checksum-2
+  [input]
+  (reduce + (map row-quotient input)))
--- test/advent_of_code_2017/day_2_test.clj
+++ test/advent_of_code_2017/day_2_test.clj
@@ -2,17 +2,35 @@
   (:require [advent-of-code-2017.day-2 :as sut]
             [clojure.test :as t]))
 
-(def example-input
+(def input (slurp (clojure.java.io/resource "day_2.txt")))
+
+; Part One
+
+(def example-1-input
   '((5 1 9 5)
     (7 5 3)
     (2 4 6 8)))
 
-(t/deftest example
-  (t/testing "Example"
-    (t/is (= (sut/checksum example-input) 18))))
+(t/deftest example-1
+  (t/testing "Example 1"
+    (t/is (= (sut/checksum example-1-input) 18))))
 
-(def input (slurp (clojure.java.io/resource "day_2.txt")))
-
-(t/deftest answer
-  (t/testing "Day 2 solution"
+(t/deftest answer-1
+  (t/testing "Day 2 part 1 solution"
     (t/is (= (sut/checksum (sut/parse-input input)) 45972))))
+
+; Part Two
+
+
+(def example-2-input
+  '((5 9 2 8)
+    (9 4 7 3)
+    (3 8 6 5)))
+
+(t/deftest example-2
+  (t/testing "Example 2"
+    (t/is (= (sut/checksum-2 example-2-input) 9))))
+
+(t/deftest answer-2
+  (t/testing "Day 2 part 2 solution"
+    (t/is (= (sut/checksum-2 (sut/parse-input input)) 326))))
