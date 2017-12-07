75e30de6f0b6a3a86ac578822dd70baf6a680e91
Matt Bray <mattjbray@gmail.com>
2017-12-07 23:07:22 +0000

    Solve day 2 part 2 in Prolog.

--- prolog/2.pl
+++ prolog/2.pl
@@ -9,7 +9,9 @@ checksum([Row|Rows], Result) :-
     checksum(Rows, Acc),
     Result is RowChecksum + Acc.
 
-?- checksum([[5,1,9,5], [7,5,3], [2,4,6,8]], 18).
+example_spreadsheet([[5,1,9,5], [7,5,3], [2,4,6,8]]).
+
+?- example_spreadsheet(Spreadsheet), checksum(Spreadsheet, 18).
 
 partition_on(List, C, Result) :-
     partition_on_inner(List, C, [], Result).
@@ -43,3 +45,36 @@ read_spreadsheet(Rows) :-
     rows_codes(RowCodes, Rows).
 
 ?- read_spreadsheet(Rows), checksum(Rows, 45972).
+
+% Part 2
+
+dividend(_,[],0).
+dividend(Dividend, [Divisor|_], Quotient) :-
+    Dividend =\= Divisor,
+    divmod(Dividend, Divisor, Quotient, 0).
+dividend(Dividend, [_|T], Quotient) :-
+        dividend(Dividend, T, Quotient).
+
+checksum_2_row_inner(Row, [Dividend|_], Quotient) :-
+    dividend(Dividend, Row, Quotient),
+    0 =\= Quotient.
+checksum_2_row_inner(Row, [_|Rest], Quotient) :-
+    checksum_2_row_inner(Row, Rest, Quotient).
+
+checksum_2_row(Row, Checksum) :-
+    checksum_2_row_inner(Row, Row, Checksum).
+
+?- checksum_2_row([5,9,2,8], 4).
+?- checksum_2_row([9,4,7,3], 3).
+?- checksum_2_row([3,8,6,5], 2).
+
+checksum_2([], 0).
+checksum_2([Row|Rows], Result) :-
+    checksum_2_row(Row, Acc1),
+    checksum_2(Rows, Acc2),
+    Result is Acc1 + Acc2.
+
+example_spreadsheet_2([[5,9,2,8], [9,4,7,3], [3,8,6,5]]).
+
+?- example_spreadsheet_2(Rows), checksum_2(Rows, 9).
+?- read_spreadsheet(Rows), checksum_2(Rows, 326).
