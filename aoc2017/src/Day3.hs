30dfd70be1016495284c783b97a6ae9f0a4e6565
Matt Bray <mattjbray@gmail.com>
2017-12-16 15:55:25 +0000

    Solve Day 3 part 2.

--- aoc2017/src/Day3.hs
+++ aoc2017/src/Day3.hs
@@ -2,11 +2,13 @@
 module Day3 where
 
 import Control.Lens
+import qualified Data.Map as M
+import Data.Maybe (mapMaybe)
 
 data Position = Position
   { _ups :: Int
   , _rights :: Int
-  } deriving (Show)
+  } deriving (Show, Eq, Ord)
 
 makeLenses ''Position
 
@@ -23,63 +25,77 @@ nextDir DUp = DLeft
 nextDir DLeft = DDown
 nextDir DDown = DRight
 
-data Acc = Acc
+data Step = Step
   { _currentPos :: Position
   , _minPos :: Position
   , _maxPos :: Position
   , _currentDir :: Direction
   } deriving (Show)
 
-makeLenses ''Acc
+makeLenses ''Step
 
-initAcc :: Acc
-initAcc =
-  Acc
+initStep :: Step
+initStep =
+  Step
   { _currentPos = Position 0 0
   , _minPos = Position 0 0
   , _maxPos = Position 0 0
   , _currentDir = DRight
   }
 
-acc = initAcc
-
-nextPos :: Acc -> Acc
-nextPos acc =
-  case acc ^. currentDir of
+nextStep :: Step -> Step
+nextStep step =
+  case step ^. currentDir of
     DRight ->
-      let acc' = acc & (currentPos . rights) +~ 1
-      in if acc' ^. currentPos . rights > acc' ^. maxPos . rights
-           then let acc'' = acc' & (maxPos . rights) +~ 1
-                in acc'' & currentDir %~ nextDir
-           else acc'
+      let step' = step & (currentPos . rights) +~ 1
+      in if step' ^. currentPos . rights > step' ^. maxPos . rights
+           then let step'' = step' & (maxPos . rights) +~ 1
+                in step'' & currentDir %~ nextDir
+           else step'
     DUp ->
-      let acc' = acc & (currentPos . ups) +~ 1
-      in if acc' ^. currentPos . ups > acc' ^. maxPos . ups
-           then let acc'' = acc' & (maxPos . ups) +~ 1
-                in acc'' & currentDir %~ nextDir
-           else acc'
+      let step' = step & (currentPos . ups) +~ 1
+      in if step' ^. currentPos . ups > step' ^. maxPos . ups
+           then let step'' = step' & (maxPos . ups) +~ 1
+                in step'' & currentDir %~ nextDir
+           else step'
     DLeft ->
-      let acc' = acc & (currentPos . rights) -~ 1
-      in if acc' ^. currentPos . rights < acc' ^. minPos . rights
-           then let acc'' = acc' & (minPos . rights) -~ 1
-                in acc'' & currentDir %~ nextDir
-           else acc'
+      let step' = step & (currentPos . rights) -~ 1
+      in if step' ^. currentPos . rights < step' ^. minPos . rights
+           then let step'' = step' & (minPos . rights) -~ 1
+                in step'' & currentDir %~ nextDir
+           else step'
     DDown ->
-      let acc' = acc & (currentPos . ups) -~ 1
-      in if acc' ^. currentPos . ups < acc' ^. minPos . ups
-           then let acc'' = acc' & (minPos . ups) -~ 1
-                in acc'' & currentDir %~ nextDir
-           else acc'
+      let step' = step & (currentPos . ups) -~ 1
+      in if step' ^. currentPos . ups < step' ^. minPos . ups
+           then let step'' = step' & (minPos . ups) -~ 1
+                in step'' & currentDir %~ nextDir
+           else step'
 
-posAfterSteps :: Int -> [Position]
-posAfterSteps n =
-  map _currentPos . take n . iterate nextPos $ initAcc
+squarePositions :: [Position]
+squarePositions = map _currentPos . iterate nextStep $ initStep
 
 manhattanDistance :: Int -> Int
 manhattanDistance n =
-  let finalPos =
-        last $ posAfterSteps n
-      in
-    finalPos ^. ups . to abs + finalPos ^. rights . to abs
+  let finalPos = last . take n $ squarePositions
+  in finalPos ^. ups . to abs + finalPos ^. rights . to abs
 
-part1 = manhattanDistance 265149
+neighbours :: Position -> [Position]
+neighbours p =
+  [ p & ups +~ 1 & rights -~ 1, p & ups +~ 1, p & ups +~ 1 & rights +~ 1
+  , p & ups +~ 0 & rights -~ 1,               p & ups +~ 0 & rights +~ 1
+  , p & ups -~ 1 & rights -~ 1, p & ups -~ 1, p & ups -~ 1 & rights +~ 1
+  ]
+
+squareValues :: [Int]
+squareValues =
+  1 : go (nextStep initStep) (M.fromList [(initStep ^. currentPos, 1)])
+  where
+    go :: Step -> M.Map Position Int -> [Int]
+    go step valuesByPosition =
+      let val =
+            sum .
+            mapMaybe (\p -> M.lookup p valuesByPosition) .
+            neighbours . view currentPos $
+            step
+      in val :
+         go (nextStep step) (M.insert (step ^. currentPos) val valuesByPosition)
--- aoc2017/test/Spec.hs
+++ aoc2017/test/Spec.hs
@@ -3,17 +3,42 @@ import Test.Tasty.HUnit
 import Day3
 
 main :: IO ()
-main = defaultMain tests
+main = defaultMain $ testGroup "Advent of Code 2017"
+  [ day3tests ]
 
-tests = testGroup "Day 3"
- [ testCase "Data from square 1 is carried 0 steps, since it's at the access port" $
-   manhattanDistance 1 @?= 0
- , testCase "Data from square 12 is carried 3 steps, such as: down, left, left" $
-   manhattanDistance 12 @?= 3
- , testCase "Data from square 23 is carried only 2 steps: up twice" $
-   manhattanDistance 23 @?= 2
- , testCase "Data from square 1024 must be carried 31 steps" $
-   manhattanDistance 1024 @?= 31
- , testCase "The solution is 438" $
-   manhattanDistance 265149 @?= 438
- ]
+day3tests =
+  testGroup
+    "Day 3"
+    [ testGroup
+        "Part 1"
+        [ testCase
+            "Data from square 1 is carried 0 steps, since it's at the access port" $
+          manhattanDistance 1 @?= 0
+        , testCase
+            "Data from square 12 is carried 3 steps, such as: down, left, left" $
+          manhattanDistance 12 @?= 3
+        , testCase "Data from square 23 is carried only 2 steps: up twice" $
+          manhattanDistance 23 @?= 2
+        , testCase "Data from square 1024 must be carried 31 steps" $
+          manhattanDistance 1024 @?= 31
+        , testCase "The solution is 438" $ manhattanDistance 265149 @?= 438
+        ]
+    , testGroup
+        "Part 2"
+        [ testCase "Square 1 starts with the value 1." $ squareValues !! 0 @?= 1
+        , testCase
+            "Square 2 has only one adjacent filled square (with value 1), so it also stores 1." $
+          squareValues !! 1 @?= 1
+        , testCase
+            "Square 3 has both of the above squares as neighbors and stores the sum of their values, 2." $
+          squareValues !! 2 @?= 2
+        , testCase
+            "Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4." $
+          squareValues !! 3 @?= 4
+        , testCase
+            "Square 5 only has the first and fourth squares as neighbors, so it gets the value 5." $
+          squareValues !! 4 @?= 5
+        , testCase "The solution is 266330" $
+          (head . dropWhile (< 265149) $ squareValues) @?= 266330
+        ]
+    ]
