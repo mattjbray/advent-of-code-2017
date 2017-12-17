835b4f415afd77851731dc60c471b827a7618450
Matt Bray <mattjbray@gmail.com>
2017-12-17 22:13:49 +0000

    IOVector.

--- aoc2017/src/Day5.hs
+++ aoc2017/src/Day5.hs
@@ -1,13 +1,15 @@
 module Day5 where
 
-import Data.Vector ((!), (//), Vector)
-import qualified Data.Vector as V
+import           Data.Vector         (Vector)
+import qualified Data.Vector         as V
+import           Data.Vector.Mutable (IOVector)
+import qualified Data.Vector.Mutable as MV
 
-jump :: (Int -> Int) -> (Int, Vector Int) -> (Int, Vector Int)
-jump f (i, v) =
-  let inst = v ! i
-      v' = v // [(i, f inst)]
-  in (i + inst, v')
+jump :: (Int -> Int) -> (Int, IOVector Int) -> IO (Int, IOVector Int)
+jump f (i, v) = do
+  inst <- MV.read v i
+  MV.modify v f i
+  return (i + inst, v)
 
 part1 :: Int -> Int
 part1 = (+) 1
@@ -18,10 +20,21 @@ part2 inst =
     then inst - 1
     else inst + 1
 
-steps :: (Int -> Int) -> Vector Int -> Int
+iterateWhileM :: Monad m => (a -> Bool) -> (a -> m a) -> a -> m [a]
+iterateWhileM cond action x =
+  if cond x
+    then do
+      next <- action x
+      xs <- iterateWhileM cond action next
+      return $ x : xs
+    else return []
+
+steps :: (Int -> Int) -> Vector Int -> IO Int
 steps f v =
-  let len = length v in
-  length . takeWhile (\(i, v) -> i < len) . iterate (jump f) $ (0, v)
+  let len = V.length v
+  in do mv <- V.thaw v
+        js <- iterateWhileM (\(i, _) -> i < len) (jump f) (0, mv)
+        return (length js)
 
 getInstructions :: IO (Vector Int)
 getInstructions =
